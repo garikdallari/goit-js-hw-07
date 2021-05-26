8673a73ac91d5417a60057f7ea21d2f4a906c8de
garikdallari <garikdallari@gmail.com>
2021-05-26 16:52:38 +0300

    task-1 modifying

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,13 @@
 const categoriesRef = document.querySelector('#categories');
-console.log(`'В списке ${categoriesRef.children.length} категории.'`);
-
-function getData(item) {
-    console.log(`Категория: ${item.firstElementChild.textContent}`);
-    console.log(`Количество элементов: ${item.lastElementChild.children.length}`);
-}
-
-getData(categoriesRef.children[0]);
-
+console.log(`В списке ${categoriesRef.children.length} категории.`);
+const getListData = list => {
+  [...list.children].map(item => {
+    const title = item.querySelector('h2').textContent;
+    const quantity = item.querySelector('ul').children.length;
+    console.log(`Категория: ${title}`);
+    console.log(`Количество элементов: ${quantity}`);
+  });
+};
+getListData(categoriesRef)
 
 
